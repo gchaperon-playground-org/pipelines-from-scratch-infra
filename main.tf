c07f75cf7ef1576f82d4ce85d8153ddaaf100975
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2024-11-20 16:40:28 -0300

    Feature/add docker registry (#3)
    * Add docker container registry
    
    * Enable artifact registry api
    
    * Remove description from components docker registry

--- main.tf
+++ main.tf
@@ -5,6 +5,21 @@ resource "google_storage_bucket" "default" {
 }
 
 resource "google_project_service" "services" {
-  for_each = toset(["aiplatform.googleapis.com"])
-  service  = each.key
+  for_each = toset([
+    "aiplatform.googleapis.com",
+    "artifactregistry.googleapis.com"
+  ])
+  service = each.key
+}
+
+resource "google_artifact_registry_repository" "components" {
+  location      = "us-central1"
+  repository_id = "components"
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = true
+  }
+
+  depends_on = [google_project_service.services]
 }
