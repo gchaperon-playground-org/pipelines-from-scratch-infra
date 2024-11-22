5c7d051452154802ef8cfe0503271a54cb6c91fd
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2024-11-22 16:20:00 -0300

    Allow vertex service agent access to components registry

--- project_infra.tf
+++ project_infra.tf
@@ -6,3 +6,16 @@ resource "google_vertex_ai_metadata_store" "default" {
   provider = google-beta
   name     = "default"
 }
+
+# AI Platform Custom Code Service Agent must be able to read artifacts from the
+# kfp components docket registry. This is poorly documented. Some references
+# about Service Agents can be found here
+# https://cloud.google.com/iam/docs/service-agents
+resource "google_project_iam_member" "artifact_reader" {
+  project = local.project
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:service-${data.google_project.default.number}@gcp-sa-aiplatform-cc.iam.gserviceaccount.com"
+}
+
+data "google_project" "default" {
+}
